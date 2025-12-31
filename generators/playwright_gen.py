c292cc0ccb6c28a9880112badb9a1163fe18d977
Victor Holanda <victorholandap@gmail.com>
2025-12-31 11:34:10 -0300

    Novos ajsute playwrightgen

--- generators/playwright_gen.py
+++ generators/playwright_gen.py
@@ -11,13 +11,7 @@ class PlaywrightGen:
         base_path = Path(output_path) / project_name
         base_path.mkdir(parents=True, exist_ok=True)
         self._create_project_structure(base_path)
-
-        for api_name, suites in project_api.items():
-            tests_path = base_path / "tests" / "api" / api_name
-            tests_path.mkdir(parents=True, exist_ok=True)
-
-            for suite, endpoints in suites.items():
-                self._create_suite_file(suite, endpoints, tests_path)
+        self._create_suite_files(project_api, base_path)
 
     def _create_project_structure(self, path: str):
         project_path = Path(path)
@@ -56,17 +50,33 @@ class PlaywrightGen:
 
         for folder in folders:
             folder.mkdir(parents=True, exist_ok=True)
+    
+    def create_payload_builder_file(self, project_api: dict[str, dict[str, list[Endpoint]]], base_path: Path):
+        for api_name, suites in project_api.items():
+            api_name = re.sub(r'[-\s\.]', "", api_name)
+            buiders_path = base_path / "src" / "builders" / api_name
+            buiders_path.mkdir(parents=True, exist_ok=True)
 
+            for suite, endpoints in suites.items():
+                file_path = buiders_path / f"{suite}PayloadBuilders.ts"
+
+                with file_path.open("w", encoding="utf-8") as f:
+                    for endpoint in endpoints:
+                        build_name = self.format_payload_build_name(endpoint.method, endpoint.path)
+
+                        f.write(f"export function {build_name}() {{\n")
+                        f.write("    return {\n")
+                        f.write("        // Define payload\n")
+                        f.write("    };\n")
+                        f.write("}\n\n")
 
     def format_payload_build_name(self, method: str, path: str) -> str:
         parts = path.strip('/').split('/')
 
         filtered = []
         for part in parts:
-            # remove chaves imediatamente
             part = part.strip('{}')
 
-            # ignora api e versões
             if part.lower() == 'api':
                 continue
             if re.match(r'v\d+', part.lower()):
@@ -74,37 +84,42 @@ class PlaywrightGen:
 
             filtered.append(part)
 
-        # capitaliza
         name = ''.join(p.capitalize() for p in filtered)
 
-        return f"build{method.upper()}{name}"
+        return f"build{method.upper()}{name}Payload"
+    
+    def _create_test_files(self, project_api: dict[str, dict[str, list[Endpoint]]], base_path: Path):
+        for api_name, suites in project_api.items():
+            api_name = re.sub(r'[-\s\.]', "", api_name)
+            tests_path = base_path / "tests" / "api" / api_name
+            tests_path.mkdir(parents=True, exist_ok=True)
 
+            for suite, endpoints in suites.items():
+                file_path = tests_path / f"{suite}.spec.ts"
+
+                with file_path.open("w", encoding="utf-8") as f:
+                    f.write("import { test, expect } from '@playwright/test';\n\n")
+                    f.write(f"test.describe('{suite}', () => {{\n\n")
+
+                    for index, endpoint in enumerate(endpoints):
+                        if index < 10:
+                            test_name = f"TC0{index+1} - {endpoint.method.upper()} {endpoint.path}"
+                        else:
+                            test_name = f"TC{index+1} - {endpoint.method.upper()} {endpoint.path}"
+
+                        payload_build_name = self.format_payload_build_name(endpoint.method, endpoint.path)
+
+                        f.write(f"  test('{test_name}', async ({{ request }}) => {{\n")
+                        f.write(f"      const payload = {payload_build_name}();\n\n")
+                        f.write(f"      const response = await request.{endpoint.method}('{endpoint.path}', {{\n")
+                        f.write(f"          data: payload,\n")
+                        f.write(f"      }});\n\n")
+                        f.write(f"      expect(response.status()).toBe(888);\n")
+                        f.write(f"  }});\n\n")
+                    f.write(f"}});\n")
             
-    def _create_suite_file(
-        self,
-        suite_name: str,
-        endpoints: list[Endpoint],
-        tests_path: Path
-    ):
-        file_path = tests_path / f"{suite_name}.spec.ts"
 
-        with file_path.open("w", encoding="utf-8") as f:
-            f.write("import { test, expect } from '@playwright/test';\n\n")
-            f.write(f"test.describe('{suite_name}', () => {{\n\n")
-
-            for index, endpoint in enumerate(endpoints):
-                if index < 10:
-                    test_name = f"TC0{index+1} - {endpoint.method.upper()} {endpoint.path}"
-                else:
-                    test_name = f"TC{index+1} - {endpoint.method.upper()} {endpoint.path}"
-
-                payload_build_name = self.format_payload_build_name(endpoint.method, endpoint.path)
-
-                f.write(f"  test('{test_name}', async ({{ request }}) => {{\n")
-                f.write(f"      const payload = {payload_build_name};\n\n")
-                f.write(f"      const response = await request.{endpoint.method}('{endpoint.path}', {{\n")
-                f.write(f"          data: payload,\n")
-                f.write(f"      }});\n\n")
-                f.write(f"      expect(response.status()).toBe(888);\n")
-                f.write(f"  }});\n\n")
-            f.write(f"}});\n")
+    def _create_suite_files(self, project_api: dict[str, dict[str, list[Endpoint]]], base_path: Path):
+        self._create_test_files(project_api, base_path)
+        self.create_payload_builder_file(project_api, base_path)
+    
